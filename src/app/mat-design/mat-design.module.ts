dde1d919e26779af546001468b8b2feb9f14b658
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2019-02-12 13:50:19 +0100

    Add more material design dependencies

---
+++ src/app/mat-design/mat-design.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import {MatCardModule, MatIconModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [MatCardModule, MatIconModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  exports: [MatCardModule, MatIconModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+})
+export class MatDesignModule { }
